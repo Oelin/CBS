3ce55558decb70eb3f44466b8374ba7212fa7f95
Oelin <42823429+Oelin@users.noreply.github.com>
2022-12-01 10:50:01 +0000

    Update AstarTimeSteps.py

--- AstarTimeSteps.py
+++ AstarTimeSteps.py
@@ -13,7 +13,7 @@ from OpenClosed import OpenHigh, OpenLow, ClosedLow
 from Heuristics import ManhattanDistance
 
 class AstarTimesteps:
-    def __init__(self, gridMap, iStart, jStart, iGoal, jGoal, vertexCons, edgeCons):
+    def __init__(self, gridMap, iStart, jStart, iGoal, jGoal, vertexCons, edgeCons, time_start, time_limit=300/1000):
         self.vertexCons = vertexCons
         self.edgeCons = edgeCons
         self.CLOSED = ClosedLow()
@@ -24,13 +24,17 @@ class AstarTimesteps:
         self.jGoal = jGoal
         self.OPEN = OpenLow()
         self.path = []
+        self.time_limit = time_limit
+        self.time_start = time_start
         
     def CheckMove(self, i1, j1, i2, j2, t):
         for obs in self.vertexCons:
+            if time.perf_counter() - self.time_start >= self.time_limit: return
             if obs[1] == t + 1 and obs[0] == (i2, j2):
                 return False
         
         for obs in self.edgeCons:
+            if time.perf_counter() - self.time_start >= self.time_limit: return
             if obs[2] == t + 1 and obs[0] == (i1, j1) and obs[1] == (i2, j2):
                 return False
                 
@@ -41,11 +45,13 @@ class AstarTimesteps:
         self.OPEN.AddNode(startNode)
     
         while not self.OPEN.isEmpty():
+            if time.perf_counter() - self.time_start >= self.time_limit: return
             s = self.OPEN.GetBestNode(self.CLOSED) 
             self.CLOSED.AddNode(s)       
             if s.i == self.iGoal and s.j == self.jGoal:
                 return (True, s, self.OPEN, self.CLOSED)
             for nbr in self.gridMap.GetNeighbors(s.i, s.j):
+                if time.perf_counter() - self.time_start >= self.time_limit: return
                 if self.CheckMove(s.i, s.j, nbr[0], nbr[1], s.g) and \
                 not self.CLOSED.WasExpanded(LowNode(coord=nbr, g=s.g + 1)):
                     nbrNode = LowNode(coord=nbr, g=s.g + 1, h=ManhattanDistance(nbr[0], nbr[1], self.iGoal, self.jGoal), \
